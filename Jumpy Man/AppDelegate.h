376bbc245ba596f6f999294f0e91345fa2a13f8b
Parham Taher <parham.taher@gmail.com>
2016-07-12 13:22:15 -0400

    Initial Commit

---
+++ Jumpy Man/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Jumpy Man
+//
+//  Created by Parham Taher on 2016-07-12.
+//  Copyright © 2016 Parham Taher. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Jumpy Man/GameScene.h
@@ -0,0 +1,13 @@
+//
+//  GameScene.h
+//  Jumpy Man
+//
+
+//  Copyright (c) 2016 Parham Taher. All rights reserved.
+//
+
+#import <SpriteKit/SpriteKit.h>
+
+@interface GameScene : SKScene
+
+@end
